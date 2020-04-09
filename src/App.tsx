c69f851fa2832c48f0afa40cec153a05c24fc0ff
Matthew Manela <mmanela@gmail.com>
2020-04-08 17:24:11 -0700

    not perfect

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Grommet,
-  ResponsiveContext
+  Nav,
+  Anchor
 } from 'grommet';
-import { BirthPlanGrid } from './BirthPlanGrid';
+import { BirthPlanGrid } from './pages/BirthPlanGrid';
 import styled from "styled-components";
 import { GlobalTheme } from './Theme';
+import { SignsOfLabor } from './pages/SignsOfLabor';
 
 const StyledMainBox = styled(Box)`
   margin-bottom: 10px;
+  margin-top: 10px;
+  margin-left:30px;
+  margin-right:30px;
 `;
+enum Pages {
+  BirthPlan,
+  LaborSigns
+}
 const App = () => {
+  const [page, setPage] = useState<Pages>(Pages.BirthPlan);
+
   return (
     <Grommet theme={GlobalTheme} full>
-      <ResponsiveContext.Consumer>
-        {size => (
-          <Box fill>
-            <StyledMainBox direction='row' flex overflow={{ horizontal: 'hidden' }}>
-              <Box flex align='center' justify='center'>
-                <BirthPlanGrid />
-              </Box>
-            </StyledMainBox>
+      <Box fill>
+        <Nav direction="row" background="white" pad="small" gap="small">
+          <Anchor onClick={() => setPage(Pages.BirthPlan)}>Birth Plan</Anchor>
+          <Anchor onClick={() => setPage(Pages.LaborSigns)}>Signs of Labor</Anchor>
+        </Nav>
+        <StyledMainBox direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align='center' justify='center'>
+            {page === Pages.BirthPlan && <BirthPlanGrid />}
+            {page === Pages.LaborSigns && <SignsOfLabor />}
           </Box>
-        )}
-      </ResponsiveContext.Consumer>
+        </StyledMainBox>
+      </Box>
     </Grommet>
   );
 

70346e45146925a52b43bdff87b8ba804eee74ac
mmanela <mmanela@users.noreply.github.com>
2020-07-20 22:11:14 +0000

    Deploying to gh-pages from  @ 47b79991e9fd4161dc78a07b2670622fcfc0837a 🚀

---
+++ precache-manifest.d8849a18f9d98abeb3c97fb3671ec904.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4416a6ab71ccc74bad7d65841a6829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d36ea88b6e4158ab99b7",
+    "url": "/static/js/2.96ae3f2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.96ae3f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e68ee0f831f0c680de1",
+    "url": "/static/js/main.fb5448a3.chunk.js"
+  },
+  {
+    "revision": "5c977367a927c7c0c2db",
+    "url": "/static/js/runtime-main.9f1e4eab.js"
+  }
+]);
